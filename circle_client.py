33d2377776a0eae76afdb1bfb0a0db2ac4cb2a05
leemacpherson <34841580+leemacpherson@users.noreply.github.com>
2017-12-27 15:03:56 -0800

    an instantiation of the Circle class
    created on the Mac, but not reflected in the internet site until i uploaded it

---
+++ circle_client.py
@@ -0,0 +1,9 @@
+from circle import *  # the ' * ' lets you access indentifiers in client program without prefixing with the module name
+
+my_circle = Circle(5)  #we are intantiating the class Circle and passing an argument of '5' as the radius
+
+print ("The radius of the circle is ", my_circle.radius)
+
+print ("The area of the circle is ", format(my_circle.get_area(), ".3f"))
+
+print ("The area formatted as an integer is ", format(int(my_circle.get_area()), "4d"))
