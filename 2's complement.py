a27f18d0e2c0a3ed4a8ee95fa298437731b72c4c
leemacpherson <leemacpherson@msn.com>
2018-03-10 14:35:08 -0800

    this is a program to calculate 2s complement
    no other statements

---
+++ 2's complement.py
@@ -0,0 +1,15 @@
+# this program takes the list of hex characters and adds them
+# then negates each bit in the binary representation, then adds one to the result
+# The binary value is then converted to hex this is what is returned
+
+# Ask for all of the hex values that will be added
+
+
+# pass the sum to the function hex2bin()
+
+
+# hex2bin()returns the binary value, so you just add one to this , then negate the value
+
+
+
+  
